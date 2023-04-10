a4c9d1e3ef7287a190bf739e20c35f1692c7c252
AdamRalph123 <adambyrne16@gmail.com>
2023-04-10 22:39:29 +0000

    Installed crispy forms and styled login and logout page

--- cartalk/settings.py
+++ cartalk/settings.py
@@ -48,13 +48,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'blog',
 ]
 
 SITE_ID = 1
 
-LOGIN_REDIRECT_URL ='/'
-LOGOUT_REDIRECT_URL ='/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
